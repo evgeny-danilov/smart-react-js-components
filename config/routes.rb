1d641138d3273febed9da614bd88bd5623dad5fd
Evgeny Danilov <jmelkor@rambler.ru>
2017-12-10 18:57:44 +0300

    Dalate debug info from money input component

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   resources :posts do
   end
+
+  resources :components, only: :index do
+    collection do
+      get :money
+      get :datatime
+    end
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
